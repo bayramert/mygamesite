cad483483d368089f753a09420750d41ba969094
Ataberk Köşger <ataberkkosger.dev@gmail.com>
2026-05-17 13:28:13 +0300

    save sistemi eklendi.

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(session({
   },
 }));
 
-app.use('/leaderboard.html', (req, res, next) => {
+app.use('/games.html', (req, res, next) => {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Pragma', 'no-cache');
   res.setHeader('Expires', '0');
